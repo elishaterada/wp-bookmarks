722da4661f9cc6009ab4fe821cddb87526889735
Elisha Terada <elishaterada@gmail.com>
2014-03-18 07:32:39 -0700

    Updated template to query for all post types

--- dist/js/templates.js
+++ dist/js/templates.js
@@ -2,11 +2,16 @@ this["JST"] = this["JST"] || {};
 
 this["JST"]["tmpls/bookmark.html"] = function(obj) {
 obj || (obj = {});
-var __t, __p = '', __e = _.escape;
+var __t, __p = '', __e = _.escape, __j = Array.prototype.join;
+function print() { __p += __j.call(arguments, '') }
 with (obj) {
-__p += '<strong>' +
-((__t = ( status )) == null ? '' : __t) +
-'</strong>\n';
+__p += '<ul>\n';
+ _.each(posts, function(p) { ;
+__p += '\n\n<li>' +
+((__t = ( p.title )) == null ? '' : __t) +
+'</li>\n\n';
+ }); ;
+__p += '\n</ul>\n';
 
 }
 return __p
