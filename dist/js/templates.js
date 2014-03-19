979af411c03a50ab7105c9ef486916c0f700cd86
Elisha Terada <elishaterada@gmail.com>
2014-03-18 22:01:39 -0700

    Improved visual presentation of data

--- dist/js/templates.js
+++ dist/js/templates.js
@@ -5,11 +5,13 @@ obj || (obj = {});
 var __t, __p = '', __e = _.escape, __j = Array.prototype.join;
 function print() { __p += __j.call(arguments, '') }
 with (obj) {
-__p += '<ul>\n';
+__p += '<ul class="bookmarks">\n    ';
  _.each(posts, function(p) { ;
-__p += '\n\n<li>' +
+__p += '\n    <li class="bookmark">\n        <a class="bookmark-link" href="" title="' +
 ((__t = ( p.title )) == null ? '' : __t) +
-'</li>\n\n';
+'"></a>\n        <div class="thumb"></div>\n        <div class="details">\n            <strong>' +
+((__t = ( p.title )) == null ? '' : __t) +
+'</strong>\n        </div>\n    </li>\n    ';
  }); ;
 __p += '\n</ul>\n';
 
