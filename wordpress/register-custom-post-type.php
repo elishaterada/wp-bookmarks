c398cc8f6261ac860855350966aa0343cdb6c39d
Elisha Terada <elishaterada@gmail.com>
2014-03-21 07:45:18 -0700

    Simplified taxonomy registration

--- wordpress/register-custom-post-type.php
+++ wordpress/register-custom-post-type.php
@@ -10,31 +10,17 @@ function wp_bookmarks_posts_init() {
     $labels = array(
         'name'               => 'Bookmarks',
         'singular_name'      => 'Bookmark',
-        'add_new'            => 'Add New Bookmark',
-        'add_new_item'       => 'Add New Bookmark',
-        'edit_item'          => 'Edit Bookmark',
-        'new_item'           => 'New Bookmark',
-        'all_items'          => 'All Bookmarks',
-        'view_item'          => 'View Bookmark',
-        'search_items'       => 'Search Bookmarks',
-        'not_found'          => 'No Bookmarks found',
-        'not_found_in_trash' => 'No Bookmarks found in Trash',
-        'parent_item_colon'  => '',
         'menu_name'          => 'WP Bookmarks'
     );
 
     $args = array(
         'labels'             => $labels,
-        'public'             => true,
-        'publicly_queryable' => true,
         'show_ui'            => true,
         'show_in_menu'       => true,
-        'query_var'          => true,
         'rewrite'            => array( 'slug' => 'bookmarks' ),
         'capability_type'    => 'post',
         'has_archive'        => true,
         'hierarchical'       => false,
-        'menu_position'      => null,
         'supports'           => array( 'title', 'editor', 'author', 'thumbnail', 'revisions', 'excerpt' )
     );
 
@@ -43,36 +29,31 @@ function wp_bookmarks_posts_init() {
     // Register custom taxonomy for wp_bookmakrs custom post
 
     $labels = array(
-        'name'               => 'Bookmark Categories',
-        'singular_name'      => 'Bookmark Category',
-        'add_new'            => 'Add New',
-        'add_new_item'       => 'Add New Bookmark Category',
-        'edit_item'          => 'Edit Bookmark Category',
-        'new_item'           => 'New Bookmark Category',
-        'all_items'          => 'All Bookmark Categories',
-        'view_item'          => 'View Bookmark Category',
-        'search_items'       => 'Search Bookmark Categories',
-        'not_found'          => 'No Bookmark Categories found',
-        'not_found_in_trash' => 'No Bookmark Categories found in Trash',
-        'parent_item_colon'  => '',
-        'menu_name'          => 'Bookmark Categories'
+        'name'               => 'Categories',
+        'singular_name'      => 'Category',
+        'menu_name'          => 'Categories'
     );
 
     $args = array(
         'labels'             => $labels,
-        'public'             => true,
-        'publicly_queryable' => true,
-        'show_ui'            => true,
-        'show_in_menu'       => true,
-        'query_var'          => true,
-        'rewrite'            => array( 'slug' => 'wp-bookmark-category' ),
-        'capability_type'    => 'post',
-        'has_archive'        => true,
+        'rewrite'            => array( 'slug' => 'restaurant-category' ),
         'hierarchical'       => true,
-        'menu_position'      => null,
-        'supports'           => array()
     );
 
-    register_taxonomy( 'wp_bookmarks_categories', 'wp_bookmarks', $args );
+    register_taxonomy( 'categories', 'wp_bookmarks', $args );
+
+    $labels = array(
+        'name'               => 'City',
+        'singular_name'      => 'City',
+        'menu_name'          => 'City'
+    );
+
+    $args = array(
+        'labels'             => $labels,
+        'rewrite'            => array( 'slug' => 'restaurant-city' ),
+        'hierarchical'       => true,
+    );
+
+    register_taxonomy( 'city', 'wp_bookmarks', $args );
 
 }
